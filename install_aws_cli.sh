84145d3fe627b3c278c3c41c3d8a403be785eea9
tungngt <tung.nguyent03@gmail.com>
2025-11-20 22:41:22 +0700

    add install

---
+++ install_aws_cli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
---
+++ install_terraform.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install terraform on fedora
+sudo dnf install -y dnf-plugins-core
+sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+sudo dnf -y install terraform
